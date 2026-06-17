e2eb0f5b3adcf454f660f7e66a8773ac6fbf57c3
Naymur Rahman <naymurmn@gmail.com>
2026-06-17 14:20:29 +0600

    query 5

--- QUERY.sql
+++ QUERY.sql
@@ -107,6 +107,31 @@ INSERT INTO bookings (booking_id, user_id, match_id, seat_number, payment_status
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
 
+ 
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
 SELECT match_id, fixture, base_ticket_price FROM matches 
   WHERE tournament_category = 'Champions League' AND match_status = 'Available';
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+SELECT user_id, full_name, email FROM users 
+  WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+
+--Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required')
+  AS systematic_status FROM bookings
+  WHERE payment_status IS NULL ;
+
+
+--Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+SELECT b.booking_id, u.full_name, m.fixture, ROUND(b.total_cost::numeric) AS "total_cost" FROM bookings AS b 
+  JOIN users AS u ON b.user_id = u.user_id 
+  JOIN matches AS m ON b.match_id = m.match_id;
+
+
+--Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+SELECT u.user_id, u.full_name, b.booking_id 
+  FROM users AS u 
+  LEFT JOIN bookings AS b 
+  ON u.user_id = b.user_id
